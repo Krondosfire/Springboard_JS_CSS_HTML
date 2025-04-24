e7deef055c0e3c942bdcfd965686cc510b15b409
Javor E Mladenoff <68168189+Krondosfire@users.noreply.github.com>
2025-04-23 23:00:49 -0400

    AJAX
    Axios

--- JavaScript/JavaScript_Intermediate/APIs/api.js
+++ JavaScript/JavaScript_Intermediate/APIs/api.js
@@ -2,3 +2,12 @@ window.addEventListener('load', function () {
     console.log(`Content is Fully Loaded!`);
 });
 
+// AJAX with Axios
+
+console.log(`*************************AJAX with Axios**********************`);
+
+console.log(axios);
+
+// https://swapi.co/api/planets/
+const response = axios.get('https://swapi.co/api/planets/');
+console.log(response);
