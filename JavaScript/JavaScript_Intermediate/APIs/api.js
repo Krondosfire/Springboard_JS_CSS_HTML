567eec77a3959bfb7987eeba6ee7cede443a28b9
Javor E Mladenoff <68168189+Krondosfire@users.noreply.github.com>
2025-04-26 17:07:49 -0400

    Axios
    APIs

--- JavaScript/JavaScript_Intermediate/APIs/api.js
+++ JavaScript/JavaScript_Intermediate/APIs/api.js
@@ -6,9 +6,58 @@ window.addEventListener('load', function () {
 
 console.log(`*************************AJAX with Axios**********************`);
 
-console.log(axios);
+// console.log(axios);
 
-// https://swapi.co/api/planets/
-const response = axios.get('https://swapi.co/api/planets/');
-console.log(response);
-console.log(axios);
+// https://swapi.info/api/planets
+
+// console.log(response);
+// console.log(axios);
+
+// Async/Await with Axios
+console.log(`*************************Async/Await with Axios**********************`);
+
+console.log(`AJAX requests are asynchronous`);
+
+
+
+async function getData() {
+    const response = await axios.get('https://swapi.info/api/planets');
+    for (let planet of response.data) {
+        console.log(planet.name);
+    }
+
+}
+
+getData();
+console.log(getData());
+
+
+async function getRandomDog() {
+    const res = await axios.get('https://dog.ceo/api/breeds/image/random');
+    console.log(res.data);
+    const img = document.querySelector("#dog");
+    img.src = res.data.message;
+}
+
+console.log(getRandomDog());
+
+async function getDogByBreed(breed) {
+   try {const url = `https://dog.ceo/api/breed/${breed}/images/random`;
+    const res = await axios.get(url);
+    const img = document.querySelector("#dog");
+    img.src = res.data.message;
+} catch (error) {
+    console.log(error)
+    alert("BREED NOT FOUND!");
+    getRandomDog();
+}
+}
+// console.log(getDogByBreed('bulldog'));
+const form = document.querySelector('#searchform');
+const input = document.querySelector('#search');
+form.addEventListener("submit", function (error) {
+    error.preventDefault();
+    getDogByBreed(input.value);
+    input.value = '';
+})
+
